001ef0569220a4d2550c511173f26a14d645602c
AK Naresh <ak@AKs-MacBook-Pro.local>
2025-12-04 10:15:59 +0000

    Refactored 'articles.model.js' and 'topics.model.js':
    - to use function declarations and module.exports instead of direct exports.

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,11 +1,14 @@
 const db = require("../db/connection");
 
-exports.fetchTopics = () => {
-  /* 
+/* 
 Queries the database to get all topics and returns a promis
 Query selects both 'slug' && 'description' from topics table, so each topic object in the result will contain both fields.
 */
+
+function fetchTopics() {
   return db
     .query(`SELECT slug, description FROM topics;`)
     .then((result) => result.rows);
-};
+}
+
+module.exports = { fetchTopics };
