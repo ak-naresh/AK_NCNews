4afd50e4ce2b35e1c92d5fddbd4fb9058c6837a9
AK Naresh <ak@AKs-MacBook-Pro-2.local>
2026-01-04 00:17:31 +0000

    Configure production environment and deployment settings

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
-
 const ENV = process.env.NODE_ENV || "development";
 const path = `${__dirname}/../.env.${ENV}`;
 require("dotenv").config({ path: path });
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
 console.log(process.env.PGDATABASE);
 
-module.exports = pool;
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
